2ff09206513a1e8ccb696a200e00a61414c69f58
urana03 <uhackninja@gmail.com>
2022-11-17 19:42:31 +0530

    Program to check the month's wage of employee

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -9,25 +9,35 @@ public class EmployeeWage
     	int check = ran.nextInt(2);
     	
     	int wagePerHr = 20;
+    	int dailyEmpWage = 0;
+    	int totalWage = 0;
+    	
     	int fullTimeHrs = 8;
     	int partTimeHrs = 4;
-    	int dailyEmpWage = 0;
     	
+    	
+    	int empWorkingDays = 20;
+    	int totalWorkingDays=0;
+    	
+    	while(totalWorkingDays<=empWorkingDays) {
     	switch(check) {
     		
     	case 0:
-    		dailyEmpWage = fullTimeHrs*wagePerHr;
-    		System.out.println("Full Time Wage of Employee:" +dailyEmpWage);
+    		 fullTimeHrs = 8;
+    		 dailyEmpWage = fullTimeHrs*wagePerHr;
+    		 totalWage = totalWage+dailyEmpWage;
     	    break;
     		
     	case 1:
-    		dailyEmpWage = partTimeHrs*wagePerHr;
-    		System.out.println("Part Time Wage of Employee:" +dailyEmpWage);
+    		 partTimeHrs = 4;
+    		 dailyEmpWage = partTimeHrs*wagePerHr;
+    		 totalWage = totalWage+dailyEmpWage;
     		break;
     	
     	}
+    	totalWorkingDays++;
     	
-    	
-    	
+    	}	
+    	System.out.println("This month's wage of employee: "+totalWage);
     }
 }
