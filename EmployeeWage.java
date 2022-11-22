dcf7b1e2be2f74d93f64eb36e41b4dd99ea83465
urana03 <uhackninja@gmail.com>
2022-11-22 13:46:46 +0530

    Add:Program to get Employee Wage using class Method

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,43 +1,60 @@
 package com.bridgelabz.employeewage;
 import java.util.Random;
 
+
 public class EmployeeWage 
 {
+	public static final int fullTimeHrs = 1;
+	public static final int partTimeHrs = 2;
+	public static final int wagePerHr = 20;
+	public static final int totalWorkingDays = 20;
+	public static final int totalWorkingHrs = 100;
+	
+	public static int computeWage(){
+		
+		int empHrs = 0, empWorkingDays=0, totalEmpHrs = 0;
+		
+		Random ran = new Random();
+    	int check = ran.nextInt(3);
+    	
+    	while(empWorkingDays<=totalWorkingDays && totalEmpHrs <= totalWorkingHrs) {
+        	switch(check) {
+        		
+        	case fullTimeHrs:
+        		 
+        		empHrs = 8;
+        		
+        	    break;
+        		
+        	case partTimeHrs:
+        		 
+        		empHrs = 4;
+        		
+        		break;
+        		
+        	default:
+       		 
+        		empHrs = 0;
+        		
+        		break;
+        	
+        	}
+        	totalEmpHrs +=empHrs;
+        	
+        	empWorkingDays++;
+        	
+        	}	
+    	    int totalEmpWage = totalEmpHrs*wagePerHr;
+        	System.out.println("This month's wage of employee: "+totalEmpWage);
+    	    return totalEmpWage;
+        }
+	
+	
     public static void main( String[] args )
     {
-    	Random ran = new Random();
-    	int check = ran.nextInt(2);
     	
-    	int wagePerHr = 20;
-    	int dailyEmpWage = 0;
-    	int totalWage = 0;
-    	
-    	int fullTimeHrs = 8;
-    	int partTimeHrs = 4;
-    	
-    	
-    	int empWorkingDays = 20;
-    	int totalWorkingDays=0;
-    	
-    	while(totalWorkingDays<=empWorkingDays) {
-    	switch(check) {
-    		
-    	case 0:
-    		 fullTimeHrs = 8;
-    		 dailyEmpWage = fullTimeHrs*wagePerHr;
-    		 totalWage = totalWage+dailyEmpWage;
-    	    break;
-    		
-    	case 1:
-    		 partTimeHrs = 4;
-    		 dailyEmpWage = partTimeHrs*wagePerHr;
-    		 totalWage = totalWage+dailyEmpWage;
-    		break;
-    	
+    	computeWage();
+    
     	}
-    	totalWorkingDays++;
-    	
-    	}	
-    	System.out.println("This month's wage of employee: "+totalWage);
-    }
+
 }
